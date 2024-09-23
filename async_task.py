970c6e171d0eecb24ee342db26a10eb874c6607d
SuosSovanrith <suos089@gmail.com>
2024-09-24 01:12:19 +0700

    complete all necessary tasks

--- async_task.py
+++ async_task.py
@@ -1,8 +1,26 @@
 import asyncio
 
-async def async_write_to_file(filename, data, duration):
+# Function to write data to a file (blocking I/O)
+def write_to_file(filename, data):
+    print(f"Starting to write to {filename}...")
     
-    pass
+    with open(filename, 'w') as f:
+        f.write(data)
+        
+    print(f"Finished writing to {filename}.")
+    
+    
+async def async_write_to_file(filename, data, duration):  
+    await asyncio.sleep(duration)
+    #  offloads the blocking file I/O operation to a thread
+    await asyncio.to_thread(write_to_file, filename, data)
 
-async def run_async_tasks():
-    pass
+
+async def run_async_tasks(data):
+    filenames = ["prime1.txt", "prime2.txt", "prime3.txt", "prime4.txt", "prime5.txt"]
+    data_str = ", ".join(map(str, data))
+    
+    tasks = [async_write_to_file(filename, data_str, 1) for filename in filenames]
+    
+    # Wait for all tasks to complete
+    await asyncio.gather(*tasks)
--- multiprocessing_task.py
+++ multiprocessing_task.py
@@ -1,10 +1,21 @@
 import multiprocessing
 
 def is_prime(n):
-  pass
+  if n <= 1:
+      return False
+  for i in range(2, int(n**0.5) + 1):
+      if n % i == 0:
+          return False
+  return True
 
 def check_prime_chunk(numbers):
-  pass
+  return [number for number in numbers if is_prime(number)]
 
 def find_primes_in_range(numbers, chunk_size):
-  pass
+  number_chunk = [numbers[i * chunk_size:(i + 1) * chunk_size] for i in range(multiprocessing.cpu_count())]
+  
+  # Use multiprocessing to check the chunks for prime
+  with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+      result = pool.map(check_prime_chunk, number_chunk)
+      
+  return [prime for chunk in result for prime in chunk]
