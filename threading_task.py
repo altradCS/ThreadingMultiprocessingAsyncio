4775f768146d14c048551df680c781fab847d38e
Kienghok <2023500Kheav@aupp.edu.kh>
2024-09-24 21:13:13 +0700

    New Update

--- threading_task.py
+++ threading_task.py
@@ -2,28 +2,36 @@ import threading
 import time
 import requests
 
-def simulate_io_task(file_name, duration):
+
+def simulate_io_task(file_name, url, duration):
     print(f"Starting download for {file_name}...")
     time.sleep(duration)  
 
-    response = requests.get('https://raw.githubusercontent.com/Kheav-Kienghok/ThreadingMultiprocessingAsyncio/refs/heads/main/numbers.txt')
+    response = requests.get(url)
     
     if response.status_code == 200:
-        numbers = response.text.splitlines()
-        print(f"Processed {len(numbers)} lines from {file_name}.")
+        with open(file_name, 'wb') as file:
+            file.write(response.content)
+        print(f"Finished downloading {file_name}.")
     else:
         print(f"Failed to download {file_name}. Status code: {response.status_code}")
     
-    print(f"Finished downloading and processing {file_name}.")
 
 def run_io_tasks():
 
-    file_names = ['number1.txt', 'number2.txt', 'number3.txt', 'number4.txt', 'number5.txt']
-    duration = 2  
     threads = []
-
-    for file_name in file_names: 
-        thread = threading.Thread(target = simulate_io_task, args = (file_name, duration))
+    duration = 2
+    
+    # URLs for the files you want to download
+    file_urls = [
+        "https://raw.githubusercontent.com/Kheav-Kienghok/CSB-Assignment/refs/heads/main/prime1.txt",
+        "https://raw.githubusercontent.com/Kheav-Kienghok/CSB-Assignment/refs/heads/main/prime2.txt",
+        "https://raw.githubusercontent.com/Kheav-Kienghok/CSB-Assignment/refs/heads/main/prime3.txt"
+    ]
+    
+    for i, url in enumerate(file_urls):
+        filename = f"down_load_prime{i + 1}.txt"  
+        thread = threading.Thread(target = simulate_io_task, args = (filename, url, duration))
         threads.append(thread)
         thread.start()
 
