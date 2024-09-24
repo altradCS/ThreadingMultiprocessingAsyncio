d2037398516e83298eeae0ca02db02758d42dc76
sophanithsom <2023468som@aupp.edu.kh>
2024-09-24 18:42:51 +0700

    AsyncThreadMultiHW1

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import multiprocessing_task
 import threading_task
 import async_task
 import generate_numbers
+import multiprocessing
+import requests
+import asyncio
 
 def main():
     # Step 1: Generate numbers file
     print("Generating numbers.txt file...")
     generate_numbers.generate_numbers_file("numbers.txt", 10000, 100000, 1000000)
+
+    # url = 'https://raw.githubusercontent.com/sophanithsom/ThreadingMultiprocessingAsyncio/refs/heads/main/numbers.txt'
+    # response = requests.get(url)
+    # with open('numbers.txt', 'w') as file:
+    #     file.write(response.text)
     
     # Step 2: Read numbers from file
     with open("numbers.txt", "r") as f:
-        numbers = [int(line.strip()) for line in f.readlines()]
+        numbers = [int(line.strip()) for line in f if line.strip()]
 
     # Step 3: Run multiprocessing task to find primes
     print("Running multiprocessing task...")
@@ -23,8 +31,7 @@ def main():
 
     # Step 5: Run async tasks
     print("Running async I/O tasks...")
-    import asyncio
-    asyncio.run(async_task.run_async_tasks())
+    asyncio.run(async_task.run_async_tasks(primes))
 
 if __name__ == "__main__":
     main()
